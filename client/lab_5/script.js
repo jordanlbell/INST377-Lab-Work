f386b129052aac8e3cce561f3f03e2fba1ffbafd
jordanlbell <jordanlbell19@gmail.com>
2023-03-03 15:50:15 -0500

    Lab 5 filterButton

--- client/lab_5/script.js
+++ client/lab_5/script.js
@@ -24,9 +24,7 @@ async function mainEvent() { // the async keyword means we can make API requests
   mainForm.addEventListener('submit', async (submitEvent) => { // async has to be declared on every function that needs to "await" something
   
 
-    filterButton.addEventListener('click', (event) => {
-
-    })
+  
     
     // This prevents your page from becoming a list of 1000 records from the county, even if your form still has an action set on it
     submitEvent.preventDefault(); 
@@ -59,6 +57,9 @@ async function mainEvent() { // the async keyword means we can make API requests
     console.table(currentList); 
   });
 
+  filterButton.addEventListener('click', (event) => {
+
+  })
 
   /*
     Now that you HAVE a list loaded, write an event listener set to your filter button
