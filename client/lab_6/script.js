4c54fcbc92397ed9dbbe0c6ecb220cfa57db8cc7
jordanlbell <jordanlbell19@gmail.com>
2023-03-17 11:09:13 -0400

    Lab 6 code finished (resto error)

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -28,8 +28,6 @@ async function mainEvent() { // the async keyword means we can make API requests
   /* We need to listen to an "event" to have something happen in our page - here we're listening for a "submit" */
   loadDataButton.addEventListener('click', async (submitEvent) => { // async has to be declared on every function that needs to "await" something
   
-    
-     
     console.log('laoding data'); 
     loadAnimation.style.display = 'inline-block';
 
