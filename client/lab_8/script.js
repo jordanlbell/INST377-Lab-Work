3eb23df1e98e072338cef611c29af2e5b06c9e09
jordanlbell <jordanlbell19@gmail.com>
2023-04-07 13:35:18 -0400

    Lab 8 final

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -38,9 +38,39 @@ function filterList(list, query) {
 
 }
 
+function initMap() {
+  const carto = L.map('map').setView([38.98, -76.93], 13);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(carto);
+  return carto;
+
+}
+
+function markerPlace(array, map) {
+
+  console.log('array for markers', array)
+
+  map.eachLayer((layer) => {
+    if (layer instanceof L.Marker) {
+      layer.remove();
+    }
+  });
+
+  array.forEach((item) => {
+    console.log('markerPlace', item);
+    const { coordinates } = item.geocoded_column_1
+    L.marker([coordinates[1], coordinates[0]]).addTo(map);
+
+  })
+
+}
+
 async function mainEvent() { // the async keyword means we can make API requests
-  const mainForm = document.querySelector('.main_form'); 
+  const mainForm = document.querySelector('.main_form');
   const loadDataButton = document.querySelector('#data_load');
+  const clearDataButton = document.querySelector('#data_clear');
   const generateListButton = document.querySelector('#generate');
   const textField = document.querySelector('#resto')
 
@@ -48,12 +78,14 @@ async function mainEvent() { // the async keyword means we can make API requests
   loadAnimation.style.display = 'none';
   generateListButton.classList.add('hidden');
 
+  const carto = initMap();
+
   const storedData = localStorage.getItem('storedData');
-  const parsedData = JSON.parse(storedData);
-  if (parsedData.length > 0){
+  let parsedData = JSON.parse(storedData);
+  if (parsedData?.length > 0) {
     generateListButton.classList.remove('hidden');
   }
- 
+
   let currentList = []; // this is "scoped" to the main event function
 
   /* We need to listen to an "event" to have something happen in our page - here we're listening for a "submit" */
@@ -67,7 +99,8 @@ async function mainEvent() { // the async keyword means we can make API requests
 
     const storedList = await results.json();
     localStorage.setItem('storedData', JSON.stringify(storedList));
-    if (storedList.length > 0) {
+    parsedData = storedList
+    if (parsedData?.length > 0) {
       generateListButton.classList.remove('hidden');
     }
     loadAnimation.style.display = 'none';
@@ -79,8 +112,9 @@ async function mainEvent() { // the async keyword means we can make API requests
   generateListButton.addEventListener('click', (event) => {
     console.log('generate new list')
     currentList = processRestaurants(parsedData);
-    console.log(currentList)
+    console.log(currentList);
     injectHTML(currentList);
+    markerPlace(currentList, carto);
   })
 
   textField.addEventListener('input', (event) => {
@@ -88,6 +122,14 @@ async function mainEvent() { // the async keyword means we can make API requests
     const newList = filterList(currentList, event.target.value);
     console.log(newList)
     injectHTML(newList);
+    markerPlace(newList, carto);
+  })
+
+
+  clearDataButton.addEventListener("click", (event) => {
+    console.log('clear browser data');
+    localStorage.clear
+    console.log('localStorage Check', localStorage.getItem("storedData"))
   })
 }
 
